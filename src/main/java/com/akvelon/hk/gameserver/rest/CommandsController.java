1fe1513b9c89380e940a57ae068db4e157413337
Konstantin Nikitin <kostya.nikitin@akvelon.com>
2017-11-24 12:07:43 +0300

    added logging

--- src/main/java/com/akvelon/hk/gameserver/rest/CommandsController.java
+++ src/main/java/com/akvelon/hk/gameserver/rest/CommandsController.java
@@ -2,16 +2,19 @@ package com.akvelon.hk.gameserver.rest;
 
 import com.akvelon.hk.gameserver.dto.CommandsInDto;
 import com.akvelon.hk.gameserver.dto.CommandsOutDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class CommandsController {
 
     @PutMapping("/commands")
     public CommandsOutDto simpleInputCommands(@RequestBody CommandsInDto commandsInDto){
+        log.debug("Putted command: "+ commandsInDto.toString());
         return new CommandsOutDto(commandsInDto.getId(), commandsInDto.getCmd());
     }
 
